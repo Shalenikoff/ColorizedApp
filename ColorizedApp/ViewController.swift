6179dae351c490293012eec4231183866fe865f8
Shalenikoff <KShalen@gmail.com>
2023-02-16 16:56:39 +0300

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Кирилл Шалеников on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
